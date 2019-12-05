3b6bd53de5f239997c1ab0f4b86d825e34d3fb1f
Josue Godinez <josuegod1998@gmail.com>
2019-12-04 21:01:55 -0600

    Creacion clase categorias

---
+++ NoteBook/classCategorias.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    public class classCategorias
+    {
+        private string nombreCategoria;
+        private string colorCategoria;
+        
+        classCategorias()
+        {
+
+        }
+
+        classCategorias(string nombreCategoria, string colorCategoria)
+        {
+            this.nombreCategoria = nombreCategoria;
+            this.colorCategoria = colorCategoria; 
+        }
+    }
+}
