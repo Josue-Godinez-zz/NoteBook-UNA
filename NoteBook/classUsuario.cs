b512706e9dae2b88b3a9f97e6ee11ac0c5af9a05
Josue Godinez <josuegod1998@gmail.com>
2019-12-04 21:21:30 -0600

    Creacion clase Usuario y Nueva Interfaz(Categoria)

---
+++ NoteBook/classUsuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    class classUsuario
+    {
+        private string nombreUsuario;
+        private string contrasenaUsuario;
+        classUsuario()
+        {
+            nombreUsuario = "";
+            contrasenaUsuario = "";
+        }
+        
+        classUsuario(string nombreUsuario, string contrasenaUsuario)
+        {
+            this.nombreUsuario = nombreUsuario;
+            this.contrasenaUsuario = contrasenaUsuario;
+        }
+
+        public void setnombreUsuario(string nombreUsuario)
+        {
+            this.nombreUsuario = nombreUsuario;
+        }
+
+        public void setcontraseñaUsuario(string contrasenaUsuario)
+        {
+            this.contrasenaUsuario = contrasenaUsuario;
+        }
+    }
+}
