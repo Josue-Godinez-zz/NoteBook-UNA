e49dc70997cbb812c8994661d0161c34ff6bb9e7
Josue Godinez <josuegod1998@gmail.com>
2019-12-08 15:48:01 -0600

    Reestructuracion Proyecto
    Cambio en Clases y Interfaz Graficas Existentetes.
    Comenzar de nuevo

---
+++ NoteBook/NoteBook/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoteBook
+{
+    public class Categoria
+    {
+        public int ColorCategoria
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int NombreCategoria
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ NoteBook/NoteBook/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoteBook
+{
+    public class Usuario
+    {
+        public int NombreUsuario
+        {
+            get;
+            set;
+        }
+
+        public int ContraseñaUsuario
+        {
+            get;
+            set;
+        }
+    }
+}
